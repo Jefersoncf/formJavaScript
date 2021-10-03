501e536f554a3af6cfa0a0b184134f50e7ecc7ad
Jeferson Ferreira <fjeferson60@gmail.com>
2021-10-02 22:41:17 -0300

    Atualizando CSS, JS com bug

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
 let id = (id) => document.getElementById(id);
-let classe = (classe) => document.getElementsByClassName(classe);
+let classes = (classes) => document.getElementsByIdName(classes);
 
-let username = id('username'),
-email = id('email'),
-password = id('password'),
-form = id('form'),
+let username = (username),
+    email = id('email'),
+    password = id('password'),
+    form = id('form'),
 
-errorMessage = classe('error'),
-successIcon = classe('bx-message-check'),
-iconError = classe('bxs-message-error');
+    errorMsg = classes('error'),
+    successIcon = classes('message-check'),
+    failureIcon = classes('message-error');
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    eng(username, 0, 'Username cannot be blank');
-    eng(email, 1, 'Email cannot be blank');
-    eng(password, 2, 'Password cannot be blank');
+    engine(username, 0, "Username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    engine(password, 2, "Password cannot be blank");
 });
 
-let eng = (id, serial, message) => {
+let engine = (id, serial, message) => {
     if(id.value.trim() === '') {
         errorMsg[serial].innerHTML = message;
         id.style.border = '2px solid red';
+
         //icons
-        iconError[serial].style.opacity = '1';
+        failureIcon[serial].style.opacity = '1';
         successIcon[serial].style.opacity = '0';
-    }else {
+    }
+
+    else {
         errorMsg[serial].innerHTML = '';
         id.style.border = '2px solid green';
+
         //icons
-        errorMsg[serial].style.opacity = '0';
+        failureIcon[serial].style.opacity = '0';
         successIcon[serial].style.opacity = '1';
     }
 }
